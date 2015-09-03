da9e312f1efc1e4360370c67bf9ec4f23b637022
Lukas Powalla <lukas.powalla@googlemail.com>
2015-09-03 20:11:09 +0200

    dynamical allcoation of storage

--- Programme/Project1.cpp
+++ Programme/Project1.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <time.h>
 
+
 /*
 Some of the comments are missing. I will comment on more steps later. You need Gnuplot to create the plotfile. 
 This program uses a Gnuplot- skript to plot. 
@@ -45,51 +46,59 @@ int main(){
 	cout << "How many Grindpoint do you want to have?"<<endl;
 	cin >> n;
 	clock_t start, finish;  //  declare start and final time
-    start = clock();
+        start = clock();
 	double h;
-	double b[n+1],c[n+1],a[n+1],x[n+1],v[n+1],btilde[n+1];	
-	//double *a= new double[n+1];
+        double *b = new double[n+1];
+        double *c = new double[n+1];
+        double *a = new double[n+1];
+        double *x = new double[n+1];
+        double *v = new double[n+1];
+        double *btilde = new double[n+1];
 	
 	
 	h=1.0/(n+1);
-	for(int i=0;i<1+n;i++){ //filling up the vektors with the right number to solve the given linear sets of equation
-		a[i]=-1;
-		b[i]=2;
-		c[i]=-1;
-		x[i]=i*h;
-		btilde[i]=h*h*(100*exp(-10*x[i]));
+
+
+
+	for(int i=0;i<n+1;i++){ //filling up the vektors with the right number to solve the given linear sets of equation
+	  		a[i]=-1.0;
+	  	b[i]=2.0;
+	        c[i]=-1.0;
+		 x[i]=i*h;
+		btilde[i]=h*h*(100.0*exp(-10.0*x[i]));
 	}
-	
 	// solving equations through kind of gaussian algorhithm, first elemination of the under-part of the thriangle (aim is a[i]=0 for i=1...n) , 
 	// Of course, you have to change btilde as well. 
 	// second, elimination of the upper part of the triangle (aim is c[i]=0 for i=0...n)
 	
+
+
 	//first
 	for(int i=0;i<n+1;i++){
 		b[i+1]=b[i+1]-c[i]*(a[i+1]/b[i]);
 		btilde[i+1]+=-(a[i+1]/b[i])*btilde[i];
 	}
 	// clear storage
-
 	//second
+
+
 	for(int i=n;i>0;i--){
 		btilde[i-1]+=-btilde[i]*(c[i-1]/b[i]);
 	}
 	// normalization of the Koeficient (be careful, btilde is from now an not the original btilde, but 
+
+
+
 	ofstream Zieldatei("Daten.txt");
 	for(int i=0;i<n+1;i++){
 		btilde[i]=btilde[i]/b[i];
-		/*cout << x[i]<< "  "<< btilde[i]<<endl;
-		cout << x[i]<<"  "<< 1-(1-exp(-10))*(h*i)-exp(-10*h*i)<<endl;
-		cout << endl;*/
 		Zieldatei << x[i]<<"  "<<btilde[i]<<"  "<<1-(1-exp(-10))*(h*i)-exp(-10*h*i)<<endl;
 	}
 	Zieldatei.close();
 	finish = clock();
-    cout<<double ( (finish - start)/(double)CLOCKS_PER_SEC )<<endl;
+        cout<<double ( (finish - start)/(double)CLOCKS_PER_SEC )<<endl;
     
-	system("start gnuplot plot1.txt");
-	
+
 	// Part c) of the Project 1
 	double epsilon[n+1];
 	double maximum=0;
@@ -105,6 +114,6 @@ int main(){
 	ofstream Zieldatei1("maximumrelativeerror_grid_n.txt");
 	Zieldatei1<<"Gridpoints  "<<"epsilonmax"<<endl<<n<<"  "<<maximum;
 	Zieldatei1.close();
-	
-}
+	delete [] a, b, c, x, v, btilde;
 
+}
